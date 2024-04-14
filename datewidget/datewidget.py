9c61f313891d13d6772d7dd1cd9c6a1789ed2aa2
Samanja Cartagena <c.samanja09@Pabs.local>
2024-04-13 20:35:19 -0400

    first commit

---
+++ datewidget/datewidget.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 20:26:54 2024
+
+@author: c.samanja09
+"""
+
+from tkinter import *
+import ttkbootstrap as tb
+
+root=tb.Window(themename="superhero")
+
+root.title("TTK Bootstrap! DateEntry")
+root.iconbitmap("images/codemy.ico")
+root.geometry('500*350')
+
+
+root.mainloop()
