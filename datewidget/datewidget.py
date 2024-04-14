f32a51167b0c0d570a01b49cc24966b5fce15b67
Samanja Cartagena <c.samanja09@Pabs.local>
2024-04-13 22:12:12 -0400

    bootstrap basics

--- datewidget/datewidget.py
+++ datewidget/datewidget.py
@@ -6,13 +6,30 @@ Created on Sat Apr 13 20:26:54 2024
 """
 
 from tkinter import *
+from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 
-root=tb.Window(themename="superhero")
+root=tb.Window(themename='darkly')
+root.title("TTK Bootstrap")
+root.geometry('500x350')
 
-root.title("TTK Bootstrap! DateEntry")
-root.iconbitmap("images/codemy.ico")
-root.geometry('500*350')
 
+counter = 0
+def changer() :
+    global counter
+    counter += 1
+    if counter % 2 ==0:
+        my_label.config(text="Hello World")
+    else:
+        my_label.config(text="Goodbye World")
+
+# Create a label
+my_label = tb.Label(text="Hello World!", font=("Helvetica",28),bootstyle="danger,inverse")
+
+my_label.pack(pady=50)
+
+my_button=tb.Button(text="Click Me!", 
+                    bootstyle="success, outline", command=changer)
+my_button.pack(pady=20)
 
 root.mainloop()
